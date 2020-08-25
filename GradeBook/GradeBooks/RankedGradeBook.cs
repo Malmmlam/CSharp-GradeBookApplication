4ab3410ac8137e37a96c5b367e15b89191437d27
Josh Franklin <malmmlam@outlook.com>
2020-08-25 11:54:08 +0200

    Add RankedGrade sorting

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,7 @@
-﻿using GradeBook.Enums;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +10,28 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var averageGrades = Students.Select(stu => stu.AverageGrade).ToList();
+            averageGrades.Sort();
+
+            if (averageGrade >= 0.80 / averageGrades.Count)
+                return 'A';
+            else if (averageGrade >= 0.60 / averageGrades.Count && averageGrade < 0.80 / averageGrades.Count)
+                return 'B';
+            else if (averageGrade >= 0.40 / averageGrades.Count && averageGrade < 0.60 / averageGrades.Count)
+                return 'C';
+            else if (averageGrade >= 0.20 / averageGrades.Count && averageGrade < 0.40 / averageGrades.Count)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
